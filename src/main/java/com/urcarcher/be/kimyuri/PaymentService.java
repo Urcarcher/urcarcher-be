e7fcd25ac9f46131f1a4fb46e4f4c88d8bb44be5
kimjuvid <kimjh2012kr@gmail.com>
2024-08-20 17:09:01 +0900

    none

--- src/main/java/com/urcarcher/be/kimyuri/PaymentService.java
+++ src/main/java/com/urcarcher/be/kimyuri/PaymentService.java
@@ -2,7 +2,6 @@ package com.urcarcher.be.kimyuri;
 
 import java.util.List;
 
-import com.urcarcher.be.jjjh.entity.StoreDBTestTable;
 
 public interface PaymentService {
 	
@@ -16,35 +15,35 @@ public interface PaymentService {
 	
 	// Entity -> DTO
 	default PaymentDTO entityToDTO(PaymentEntity entity) {
-		PaymentDTO dto = PaymentDTO.builder()
-				.paymentId(entity.getPaymentId())
-				.paymentPrice(entity.getPaymentPrice())
-				.paymentDate(entity.getPaymentDate())
-                .cardId(entity.getCard().getCardId())
-                .store_id(entity.getStore().getStore_id())
-				.store_name(entity.getStore().getStore_name())
-				.category_code(entity.getStore().getCategory_code())
-				.build();
-		return dto;
+//		PaymentDTO dto = PaymentDTO.builder()
+//				.paymentId(entity.getPaymentId())
+//				.paymentPrice(entity.getPaymentPrice())
+//				.paymentDate(entity.getPaymentDate())
+//                .cardId(entity.getCard().getCardId())
+//                .store_id(entity.getStore().getStore_id())
+//				.store_name(entity.getStore().getStore_name())
+//				.category_code(entity.getStore().getCategory_code())
+//				.build();
+		return null;
 	}
 	
 	
 	// DTO -> Entity
 	default PaymentEntity dtoToEntity(PaymentDTO dto) {
 		
-		PaymentEntity entity = PaymentEntity.builder()
-				.paymentId(dto.getPaymentId())
-				.paymentPrice(dto.getPaymentPrice())
-				.paymentDate(dto.getPaymentDate())
-				.card(CardEntity.builder().cardId(dto.getCardId()).build())
-				.store(StoreDBTestTable.builder()
-				        .store_id(dto.getStore_id())
-				        .store_name(dto.getStore_name())
-				        .category_code(dto.getCategory_code())
-				        .build())
-	            
-				.build();
-		return entity;
+//		PaymentEntity entity = PaymentEntity.builder()
+//				.paymentId(dto.getPaymentId())
+//				.paymentPrice(dto.getPaymentPrice())
+//				.paymentDate(dto.getPaymentDate())
+//				.card(CardEntity.builder().cardId(dto.getCardId()).build())
+//				.store(StoreDBTestTable.builder()
+//				        .store_id(dto.getStore_id())
+//				        .store_name(dto.getStore_name())
+//				        .category_code(dto.getCategory_code())
+//				        .build())
+//	            
+//				.build();
+		return null;
 	}
 	
 	
