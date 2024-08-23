63aebcc1d63a90df034fba433dcd955ef3c62d87
jung hyun <jh441122jh@gmail.com>
2024-08-23 14:16:47 +0900

    Feat:2024-08-23 2:16pm be

--- src/main/java/com/urcarcher/be/blkwntr/TestController.java
+++ src/main/java/com/urcarcher/be/blkwntr/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -60,6 +61,7 @@ public class TestController {
 	JSONObject test5() {
 		return ExchangeRateService.getRateInfoByExType(ExchangeType.USD);
 	}
+
 }
 
 
