9a365ae8729dfd0466b7a697c64a2ec185e7005b
hswimming <sy_0826@naver.com>
2024-08-25 04:05:14 +0900

    feat : 예약 스케줄러, 예약 삭제, 환전 내역 전체 조회, 상세 조회 구현 완료 (2024-08-25, 04:05)

--- src/main/java/com/urcarcher/be/swimming/service/ExchangeService.java
+++ src/main/java/com/urcarcher/be/swimming/service/ExchangeService.java
@@ -29,9 +29,14 @@ public interface ExchangeService {
 	// 예약 내역 삭제
 	public void setDelete(Long setId);
 	
+	// 환전 내역 전체 조회
+	public List<ExchangeInfoDTO> infoList(Long cardId, String memberId);
+	
+	// 환전 내역 상세 조회
+	public ExchangeInfoDTO infoDetail(Long exId);
+	
 	// 바로 환전 DTO -> Entity
 	default ExchangeInfoEntity infoDtoToEntity(ExchangeInfoDTO infoDto) {
-
 		Member memberEntity = Member.builder()
 				.memberId("bleakwinter")
 				.build();
@@ -59,6 +64,7 @@ public interface ExchangeService {
 				.exId(infoEntity.getExId())
 				.cardId(infoEntity.getCard().getCardId())
 				.cardUsage(infoEntity.getCard().getCardType().getCardUsage())
+				.setId(infoEntity.getExchangeSet().getSetId())
 				.exRate(infoEntity.getExRate())
 				.exCur(infoEntity.getExCur())
 				.exPay(infoEntity.getExPay())
